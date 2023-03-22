585d73d380acff8a2e427346f3de22cfd88dd9b1
Tshepiso <desireetsotetsi16@gmail.com>
2023-03-22 15:07:17 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -32,6 +32,7 @@ void print_to_98(int n)
 				printf(". ");
 		}
 		else if (n > 98)
+	}
 		{
 			for (i = n ; i >= 98 ; i++)
 			{
@@ -43,7 +44,6 @@ void print_to_98(int n)
 		}
 		else if (n == 98)
 				printf("%d", n);
-	}
 }
 
 
